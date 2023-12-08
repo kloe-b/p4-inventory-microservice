64588a045ccea6f6b1c1f1dd226c496af71de1ef
kloe-b <burazerikloena@gmail.com>
2023-12-08 18:12:51 +0700

    opentelementry jaeger and prometheus

--- inventory-service/src/app.py
+++ inventory-service/src/app.py
@@ -7,9 +7,41 @@ import requests
 import json
 import threading
 import logging
+from opentelemetry import trace
+from prometheus_client import Counter, generate_latest
+from flask import Response
+from flask import Response
+from opentelemetry import trace
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.semconv.resource import ResourceAttributes
 
+
+service_name = "my-inventory-service" 
+resource = Resource(attributes={
+    ResourceAttributes.SERVICE_NAME: service_name
+})
 app = Flask(__name__)
-    
+
+trace.set_tracer_provider(TracerProvider(resource=resource))
+
+otlp_exporter = OTLPSpanExporter(
+    endpoint="http://localhost:4317",  
+    insecure=True
+)
+
+trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(otlp_exporter))
+
+FlaskInstrumentor().instrument_app(app)
+
+tracer = trace.get_tracer(__name__)
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:%(name)s: %(message)s')
+logger = logging.getLogger(__name__) 
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] =\
         'sqlite:///' + os.path.join(basedir, 'database.db')
@@ -21,65 +53,119 @@ REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
 REDIS_PORT = int(os.environ.get('REDIS_PORT', 6379))
 r = Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
+inventory_failure_counter = Counter('inventory_failure_total', 'Total number of failed inventory changes')
+
+@app.route('/metrics')
+def serve_metrics():
+    return Response(generate_latest(), mimetype="text/plain")
+
 @app.route('/inventory/<int:product_id>', methods=['GET', 'PUT'])
 def manage_inventory(product_id):
-    if request.method == 'GET':
-        item = Item.query.filter_by(id=product_id).first()
-        if item:
-            return jsonify(item.to_dict())
-        return jsonify({'error': 'Item not found'}), 404
+    with tracer.start_as_current_span("manage_inventory") as span:
+        span.set_attribute("product_id", product_id)
+        logger.info(f"Received request to manage inventory for product_id: {product_id}")
+        if request.method == 'GET':
+            item = Item.query.filter_by(id=product_id).first()
+            if item:
+                return jsonify(item.to_dict())
+            return jsonify({'error': 'Item not found'}), 404
 
-    if request.method == 'PUT':
-        data = request.json
-        item = Item.query.filter_by(id=product_id).first()
-        if item:
-            item.quantity += data.get('quantity_change', 0)
-            db.session.commit()
-            return jsonify(item.to_dict())
-        return jsonify({'error': 'Item not found'}), 404
+        if request.method == 'PUT':
+            data = request.json
+            logger.info(f"Updating inventory for product_id {product_id} with data: {data}")
+            item = Item.query.filter_by(id=product_id).first()
+            if item:
+                item.quantity += data.get('quantity_change', 0)
+                db.session.commit()
+                return jsonify(item.to_dict())
+            return jsonify({'error': 'Item not found'}), 404
 
 def handle_payment_event(message):
-    data = json.loads(message['data'])
-    order_id = data['order_id']
-    status = data['status']
-    product_id = data['product_id']  # Assuming this is included in the event data
-    quantity = data['amount']
-    # product_id, quantity = extract_order_details(order_id)  
+    with tracer.start_as_current_span("handle_payment_event"):
+            logger.info("Starting payment status listener")
+            data = json.loads(message['data'])
+            logger.info(f"Received data: {data}")
 
-    if status == 'SUCCESS':
-        confirm_inventory_reservation(product_id, quantity)
-    else:
-        release_inventory_reservation(product_id, quantity)
+            order_id = data['order_id']
+            status = data['status']
+            product_id = data['product_id']  
+            quantity = data['amount']
 
-def confirm_inventory_reservation(product_id, quantity):
+            if status == 'SUCCESS':
+                confirm_inventory_reservation(order_id,product_id, quantity)
+            else:
+                release_inventory_reservation(product_id, quantity)
+
+def confirm_inventory_reservation(order_id,product_id, quantity):
     with app.app_context():
-        item = Item.get_or_create(product_id)
-        # db.session.get(Item,product_id)
-        if item:
-            item.quantity -= quantity  
-            db.session.commit()
-            print(f"Payment sucess event received: ")
+        with tracer.start_as_current_span("confirm_inventory_reservation") as span:
+            logger.info(f"Confirming inventory reservation for order_id {order_id}, product_id {product_id}")
+            item = Item.get_or_create(product_id)
+            if item:
+                if item.quantity<quantity:
+                    print(f"no stock left: ")
+                    inventory_failure_counter.inc()
+                    r.publish('inventory_failure', json.dumps({'order_id':order_id , 'status': 'ofs', 'product_id': product_id}))
+                else:   
+                    item.quantity -= quantity  
+                    db.session.commit()
+                    r.publish('inventory_update', json.dumps({'order_id':order_id , 'status': 'reserved', 'product_id': product_id,'quantity':quantity}))
 
 def release_inventory_reservation(product_id, quantity):
+    logger.info(f"Releasing inventory reservation for product_id {product_id}")
     item = Item.query.filter_by(id=product_id).first()
     if item:
         item.quantity += quantity
         db.session.commit()
-        print(f"Payment not sucess event received: ")
+        r.publish('inventory_update', json.dumps({'order_id':order_id , 'status': 'open', 'product_id': product_id}))
+           
+
+def handle_delivery_event(message):
+    with tracer.start_as_current_span("handle_delivery_event"):
+        logger.info("Starting delivery status listener")
+        data = json.loads(message['data'])
+        order_id = data['order_id']
+        product_id = data['product_id']
+        quantity = data['quantity']
+        status = data['status']
+
+        if status == 'FAILED':
+            handle_delivery_failure(product_id,quantity)
+
+
+def handle_delivery_failure(product_id):
+    with app.app_context():
+        item = Item.query.filter_by(id=product_id).first()
+        if item:
+            item.quantity += quantity  
+            db.session.commit()
+            r.publish('inventory_failure', json.dumps({'order_id':order_id , 'status': 'delivery', 'product_id': product_id}))
+            print(f"Delivery failure for product {product_id}, inventory adjusted.")
 
 def start_listeners():
-    pubsub = r.pubsub()
-    pubsub.subscribe(**{'payment_status': handle_payment_event})
-    for message in pubsub.listen():
-        if message['type'] == 'message':
-            handle_payment_event(message)
+    with tracer.start_as_current_span("start_listeners"):
+        pubsub = r.pubsub()
+        pubsub.subscribe(**{
+            'payment_status': handle_payment_event,
+            'delivery_status': handle_delivery_event  
+        })
+        logger.info("Starting listeners")
+        for message in pubsub.listen():
+            if message['type'] == 'message':
+                if message['channel'] == 'payment_status':
+                    handle_payment_event(message)
+                elif message['channel'] == 'delivery_status':
+                    handle_delivery_event(message)
+
 
 if __name__ == '__main__':
+    logger.info("Starting Flask application and initializing database")
     with app.app_context():
         db.create_all()
-
-    listener_thread = threading.Thread(target=start_listeners)
-    listener_thread.start()
+    logger.info("Database initialized")
+    thread = threading.Thread(target=start_listeners)
+    thread.start()
+    logger.info("Background thread for payment status listener started")
 
     app.run(debug=True, port=5002)
 
--- inventory-service/src/database.py
+++ inventory-service/src/database.py
@@ -7,11 +7,13 @@ bcrypt = Bcrypt()
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
     quantity = db.Column(db.Integer, nullable=False,default=100)
 
     def to_dict(self):
         return {
             'id': self.id,
+            'name':self.name,
             'quantity': self.quantity
         }
 
@@ -19,7 +21,7 @@ class Item(db.Model):
     def get_or_create(item_id):
         item = Item.query.get(item_id)
         if not item:
-            item = Item(id=item_id)
+            item = Item(id=item_id,name='token')
             db.session.add(item)
             db.session.commit()
         return item
